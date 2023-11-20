3ffeb1130a946d92ca3a6020fad51df835a6cf04
TataruBogdan <tatarubog@gmail.com>
2023-11-20 12:17:45 +0100

    Account Deposit ... added ...

---
+++ banking-deposit-accounts-service/src/main/java/com/intecbrussel/bankingdepositaccountsservice/BankingDepositAccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.intecbrussel.bankingdepositaccountsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDepositAccountsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDepositAccountsServiceApplication.class, args);
+	}
+
+}
---
+++ banking-deposit-accounts-service/src/test/java/com/intecbrussel/bankingdepositaccountsservice/BankingDepositAccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intecbrussel.bankingdepositaccountsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDepositAccountsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
